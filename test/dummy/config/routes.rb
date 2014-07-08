04f24750e6176bda41686edc25a2586ef3d79ea3
Mark Lanett <mark.lanett@gmail.com>
2014-07-07 23:17:57 -0700

    Added dummy test app

---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  # Twigs has no routes
+end
---
+++ test/twigs_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TwigsTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Twigs
+  end
+end
